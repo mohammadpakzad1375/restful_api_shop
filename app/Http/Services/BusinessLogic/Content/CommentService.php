29486f34d886903102f3308f531d1a4bd85e4ff1
mohammadpakzad1375 <mohammad.pakzad1375@gmail.com>
2026-05-15 22:18:31 +0330

    add CommentService and develop showAllComments method

---
+++ app/Http/Services/BusinessLogic/Content/CommentService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Services\BusinessLogic\Content;
+
+use App\Http\Services\BusinessLogic\Tools\ServiceResult;
+use App\Http\Services\BusinessLogic\Tools\ServiceWrapper;
+use App\Models\Content\Comment;
+use App\Models\Content\Post;
+
+class CommentService
+{
+    public function showAllComments(): ServiceResult
+    {
+        return app(ServiceWrapper::class)(function (){
+
+            return Comment::where('commentable_type', Post::class)->orderBy('created_at','desc')->paginate(10);
+
+        });
+    }
+}
