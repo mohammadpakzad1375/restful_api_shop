f64d1ed1cf1bbbbbe23d0743c332fae61e8a1c89
mohammadpakzad1375 <mohammad.pakzad1375@gmail.com>
2026-05-15 22:23:06 +0330

    add Content/CommentApiResourceCollection

---
+++ app/Http/Resources/Content/Comment/CommentApiResourceCollection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Content\Comment;
+
+use App\Http\Services\RestfulApi\Resource\ApiResourceCollection;
+use Illuminate\Http\Request;
+
+
+class CommentApiResourceCollection extends ApiResourceCollection
+{
+    public $collects = CommentApiResource::class;
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+
+}
