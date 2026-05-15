7a93a3ca3d85d9da913a6cbe0339838934cde105
mohammadpakzad1375 <mohammad.pakzad1375@gmail.com>
2026-05-15 22:26:02 +0330

    develop Content/CommentController index method

--- app/Http/Controllers/Api/Admin/Content/CommentController.php
+++ app/Http/Controllers/Api/Admin/Content/CommentController.php
@@ -3,22 +3,34 @@
 namespace App\Http\Controllers\Api\Admin\Content;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Content\Comment\CommentApiResourceCollection;
+use App\Http\Services\BusinessLogic\Content\CommentService;
+use App\Http\Services\RestfulApi\Facades\ApiResponse;
+use App\Models\Content\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
+    public function __construct(private CommentService $commentService)
+    {
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $result = $this->commentService->showAllComments();
+
+        return ApiResponse::withData(CommentApiResourceCollection::make($result->data))
+            ->build()
+            ->response($result->success);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Comment $comment)
     {
         //
     }
@@ -31,12 +43,12 @@ class CommentController extends Controller
         //
     }
 
-    public function approved(Request $request, string $id)
+    public function approved(Request $request, Comment $comment)
     {
         //
     }
 
-    public function status(Request $request, string $id)
+    public function status(Request $request, Comment $comment)
     {
         //
     }
