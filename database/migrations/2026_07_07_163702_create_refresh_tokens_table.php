aabf51856a44a601ea47b8d3a18d872a59b65255
mohammadpakzad1375 <mohammad.pakzad1375@gmail.com>
2026-07-07 16:52:05 +0330

    add refresh_tokens table.

---
+++ database/migrations/2026_07_07_163702_create_refresh_tokens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('refresh_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('token', 255);
+            $table->ipAddress('ip_address')->nullable();
+            $table->text('user_agent')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->useCurrent()->index('expires_at');
+            $table->timestamp('revoked_at')->nullable()->index('revoked_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('refresh_tokens');
+    }
+};
