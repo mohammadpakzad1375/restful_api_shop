f3d8d2d1b489be8e330509c32e004c890418ca16
mohammadpakzad1375 <mohammad.pakzad1375@gmail.com>
2026-07-08 15:02:07 +0330

    Add RefreshTokenService.php and set jwt ttl config.

---
+++ app/Http/Services/Auth/RefreshTokenService.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Services\Auth;
+
+use App\Models\Auth\RefreshToken;
+use App\Models\User\User;
+use Illuminate\Support\Facades\DB;
+
+class RefreshTokenService
+{
+    //Refresh token EXP
+    private const REFRESH_TOKEN_TTL = 30;
+
+    //Generate refresh token
+    private function generateToken(): string
+    {
+        return bin2hex(random_bytes(32));
+    }
+
+    //Hash refresh token
+    private function hashToken(string $token): string
+    {
+        return hash('sha256', $token);
+    }
+
+    //Create new refresh token
+    public function create(User $user, ?string $ipAddress = null, ?string $userAgent = null): array {
+
+        $plainToken = $this->generateToken();
+
+        $refreshToken = RefreshToken::create([
+            'user_id'=> $user->id,
+            'token'=> $this->hashToken($plainToken),
+            'ip_address'=> $ipAddress,
+            'user_agent'=> $userAgent,
+            'expires_at'=> now()->addDays(self::REFRESH_TOKEN_TTL),
+            'revoked_at'=> null,
+        ]);
+
+        return [
+            'plain_text_token' => $plainToken,
+            'model' => $refreshToken,
+        ];
+    }
+
+    //Find valid refresh token
+    public function findValid(string $plainToken): RefreshToken|null
+    {
+        return RefreshToken::active()->where('token', $this->hashToken($plainToken))->first();
+    }
+
+   //Rotation
+    public function rotate(RefreshToken $refreshToken): array
+    {
+        return DB::transaction(function () use ($refreshToken) {
+
+            $this->revoke($refreshToken);
+
+            return $this->create(
+                $refreshToken->user,
+                $refreshToken->ip_address,
+                $refreshToken->user_agent
+            );
+        });
+    }
+
+    //Revoke a single refresh token
+    public function revoke(RefreshToken $refreshToken): void
+    {
+        if ($refreshToken->revoked_at !== null)
+            return;
+
+        $refreshToken->update([
+            'revoked_at' => now(),
+        ]);
+    }
+
+    //Revoke all refresh tokens for using in logout all devices
+    public function revokeAll(User $user): void
+    {
+        $user->refreshTokens()->active()->update([
+                'revoked_at' => now(),
+            ]);
+    }
+
+    //delete expired and revoked refresh tokens using in a queued job
+    public function cleanup()
+    {
+        return RefreshToken::whereNotNull('revoked_at')
+            ->where('expires_at', '<', now())
+            ->delete();
+    }
+}
